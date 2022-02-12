db1cb86d044ae1c07350c53078d41975e91c101b
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-02-12 09:07:59 +0300

    generated search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PortfolioComponent
+    PortfolioComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
