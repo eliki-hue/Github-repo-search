742f87d56c37e504d1a108f4b137cfd51481a2a0
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-02-14 07:47:07 +0300

    added 'Github search' and 'My Portfolio' router links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { UserComponent } from './user/user.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'portfolio',component:PortfolioComponent},
+  {path:'Github search',component:UserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
